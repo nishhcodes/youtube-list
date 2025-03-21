85d9b8d262332e57ebac2e5dee28ee601c2d6fa0
nishhcodes <issacneedscoffee@gmail.com>
2025-03-22 01:27:52 +0530

    Final commit

---
+++ src/pages/Components/page.jsx
@@ -0,0 +1,46 @@
+export default function Content() {
+  return (
+    <>
+      <div>
+        <h1 className="text-2xl">Some of my Favourite Youtubers.</h1>
+        <ul>
+          <li>
+            <a target="e_blank" href="https://www.youtube.com/@Acousticians">
+              Acoustician
+            </a>
+          </li>
+          <li>
+            <a target="e_blank" href="https://www.youtube.com/@BroCodez">
+              Bro Code
+            </a>
+          </li>
+          <li>
+            <a target="e_blank" href="https://www.youtube.com/@ColorCode-io">
+              Color Code
+            </a>
+          </li>
+          <li>
+            <a target="e_blank" href="https://www.youtube.com/@mav">
+              Mav
+            </a>
+          </li>
+          <li>
+            <a target="e_blank" href="https://www.youtube.com/@OutdoorBoys">
+              Outdoor Boys
+            </a>
+          </li>
+          <li>
+            <a target="e_blank" href="https://www.youtube.com/@TheCodingTrain">
+              Coding Train
+            </a>
+          </li>
+          <li>
+            <a target="e_blank" href="https://www.youtube.com/@VinaiT">
+              Vinay T Music
+            </a>
+          </li>
+        </ul>
+      </div>
+    </>
+  );
+}
--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,14 @@
 import "@/styles/globals.css";
+import Head from "next/head";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <title>Youtuber List | App</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
